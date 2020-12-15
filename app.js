f38dbf63760ffc083bcce55142869b6d867736cd
aditya6521 <74780249+aditya6521@users.noreply.github.com>
2020-12-15 13:47:57 +0530

    updated minion api

--- app.js
+++ app.js
@@ -3,7 +3,12 @@
     var outputText=document.querySelector("#output");
 
 
-    var serverUrl="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+    // yoda mock api by tanay sir
+    // var serverUrl="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+    //minion api
+    var serverUrl="https://api.funtranslations.com/translate/minion.json";
+
 
     function getTranslationUrl(text){
         return serverUrl + "?" + "text=" + text
